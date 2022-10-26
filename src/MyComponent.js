289336658e65a572fe9313a971959f23edee6542
okdaae <dhrekdo5@kakao.com>
2022-10-27 00:11:57 +0900

    first commit

---
+++ src/MyComponent.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const MyComponent = () =>{
+    // const Section1Ptag = document.querySelector('p');
+    // const greeting = "it is easy to register your compani"
+    // Section1Ptag.innerHTML = greeting;
+    return(
+    <div>
+        <h1>The<span>fatest<br/>than lignt</span>delivery<br/> other system to you<br/>
+        next custmers
+        </h1>
+        <p>it is easy to register your compani</p>
+        <div>
+            <button className="">register you company now</button>
+        </div>
+    </div>
+    )
+};
+//피라미터로 전달
+export default MyComponent;
